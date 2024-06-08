cc63be33e36645d134e86168ec941f992d3e9aab
Barry Simons <linuxuser586@gmail.com>
2024-06-07 20:21:50 -0400

    chore: add zap config
    Signed-off-by: Barry Simons <linuxuser586@gmail.com>

--- loggerzap/loggerzap.go
+++ loggerzap/loggerzap.go
@@ -1,41 +1,83 @@
-// Copyright 2024 The OpenMail Authors
+// Copyright The OpenMail Authors
 // SPDX-License-Identifier: Apache-2.0
 
-// Package loggerzap is a thin wrapper around zap that implements Logger.
+// Package loggerzap is a thin wrapper around [go.uber.org/zap] that implements [logger.Logger].
 package loggerzap
 
 import (
 	"fmt"
 
 	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
 
 	"github.com/linuxuser586/openmail/logger"
 )
 
-// Environment is used to provide reasonable configuration defaults
-type Environment int
+// Encoding enum
+type Encoding string
 
 const (
-	Development Environment = iota
-	Production
+	Console Encoding = "console"
+	Json    Encoding = "json"
 )
 
-// Level is the verbosity for logging
-type Level int
+func (e Encoding) String() string {
+	return string(e)
+}
 
-const (
-	Debug Level = iota
-	Info
-	Warn
-	Error
-	Panic
-	Fatal
-)
+// Config data
+type Config struct {
+	Env        logger.Environment
+	Level      logger.Level
+	Encoding   Encoding
+	MessageKey string
+}
 
-// Config provides options for creating a new Logger instance
-type Config interface {
-	GetEnvironment() Environment
-	GetLevel() Level
+func (c Config) GetEnvironment() logger.Environment {
+	return c.Env
+}
+
+func (c Config) GetLevel() logger.Level {
+	return c.Level
+}
+
+// NewDevConfig creates a reasonable development config
+func NewDevConfig() Config {
+	return Config{
+		Env:        logger.Development,
+		Level:      logger.Info,
+		Encoding:   Console,
+		MessageKey: "msg",
+	}
+}
+
+// NewProdConfig creates a reasonable production config
+func NewProdConfig() Config {
+	return Config{
+		Env:        logger.Production,
+		Level:      logger.Info,
+		Encoding:   Json,
+		MessageKey: "short_message",
+	}
+}
+
+func (c Config) toLevel() zapcore.Level {
+	switch c.Level {
+	case logger.Debug:
+		return zapcore.DebugLevel
+	case logger.Info:
+		return zapcore.InfoLevel
+	case logger.Warn:
+		return zapcore.WarnLevel
+	case logger.Error:
+		return zapcore.ErrorLevel
+	case logger.Panic:
+		return zapcore.PanicLevel
+	case logger.Fatal:
+		return zapcore.FatalLevel
+	default:
+		return zapcore.InfoLevel
+	}
 }
 
 // ZapLogger is the Logger implementation using zap
@@ -92,37 +134,55 @@ func (z ZapLogger) Fatalf(format string, a any) {
 	z.logger.Fatal(fmt.Sprintf(format, a), z.fields...)
 }
 
-func (z ZapLogger) Sync() {
-	z.logger.Sync()
-}
-
 func (z ZapLogger) WithField(key string, val any) logger.Logger {
 	z.fields = append(z.fields, zap.Any(key, val))
 	return z
 }
+
+func (z ZapLogger) Sync() {
+	z.logger.Sync()
+}
+
+func (z ZapLogger) GetLevel() logger.Level {
+	l := z.logger.Level()
+	switch l {
+	case zapcore.DebugLevel:
+		return logger.Debug
+	case zapcore.InfoLevel:
+		return logger.Info
+	case zapcore.WarnLevel:
+		return logger.Warn
+	case zap.ErrorLevel:
+		return logger.Error
+	case zapcore.DPanicLevel, zap.PanicLevel:
+		return logger.Panic
+	case zapcore.FatalLevel:
+		return logger.Fatal
+	default:
+		z.Panicf("unknown log level %v", l)
+	}
+
+	panic("does not reach")
+}
+
+// New creates a [go.uber.org/zap] instance
 func New(config Config) logger.Logger {
 	cfg := zap.NewDevelopmentEncoderConfig()
-	level := zap.DebugLevel
-	dev := true
-	encoding := "console"
 	var sampling *zap.SamplingConfig
-	if config.GetEnvironment() == Production {
+	if config.Env == logger.Production {
 		cfg = zap.NewProductionEncoderConfig()
-		level = zap.InfoLevel
-		dev = false
-		encoding = "json"
 		sampling = &zap.SamplingConfig{
 			Initial:    100,
 			Thereafter: 100,
 		}
 	}
-	cfg.MessageKey = "short_message"
+	cfg.MessageKey = config.MessageKey
 
 	c := zap.Config{
-		Level:            zap.NewAtomicLevelAt(level),
-		Development:      dev,
+		Level:            zap.NewAtomicLevelAt(config.toLevel()),
+		Development:      config.Env == logger.Development,
 		Sampling:         sampling,
-		Encoding:         encoding,
+		Encoding:         config.Encoding.String(),
 		EncoderConfig:    cfg,
 		OutputPaths:      []string{"stderr"},
 		ErrorOutputPaths: []string{"stderr"},
@@ -130,7 +190,7 @@ func New(config Config) logger.Logger {
 
 	logger, err := c.Build(zap.AddCallerSkip(1))
 	if err != nil {
-		panic((err))
+		panic(err)
 	}
 	return &ZapLogger{logger: logger}
 }
